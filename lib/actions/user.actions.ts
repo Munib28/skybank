4747621b61c63cc696a2c08157be731353041298
Munib <munib.obradovic@gmail.com>
2024-09-03 13:51:11 +0200

    Finish Sign-up and Sign-in page

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    // Mutation / Databese / Make fetch
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // Mutation / Databese / Make fetch
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
